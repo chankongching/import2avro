a008c99df5b1124260aa3be990cfc5a0bd4f9962
Jacky Chan <chankongching@gmail.com>
2019-09-03 19:16:11 +0800

    test by lesser data

--- import2.py
+++ import2.py
@@ -20,13 +20,7 @@ data = {
   "is_coinbase": False,
   "inputs": [{
     "index": 0,
-    "spent_transaction_hash": "277f770ccce6dade20a9d797e3f88419435f465db56bd7b0a4ee94bb41a069e2",
-    "spent_output_index": 1,
-    "script_asm": "304402206d077981cdbd88578439a18653ae9ded88e73f903db8dfebd9a97389a33a43a502201bfe9443fe6550c36016c26c9e459af44bcfc6e4fbdbb0b7fedbf93904c7325e[ALL] 034af10276b44b10c31e4dcd5f6e6184a33151abcee75bcb369b175eaa349cf550",
-    "script_hex": "47304402206d077981cdbd88578439a18653ae9ded88e73f903db8dfebd9a97389a33a43a502201bfe9443fe6550c36016c26c9e459af44bcfc6e4fbdbb0b7fedbf93904c7325e0121034af10276b44b10c31e4dcd5f6e6184a33151abcee75bcb369b175eaa349cf550",
-    "sequence": 4294967294,
-    "required_signatures": 1,
-    "type": "pubkeyhash"
+    "spent_transaction_hash": "277f770ccce6dade20a9d797e3f88419435f465db56bd7b0a4ee94bb41a069e2"
   }]
 }
 
